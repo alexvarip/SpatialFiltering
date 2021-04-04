8017a7974bcaa9da479eba1b801dc54ba63f8d02
alex varipatis <alexvarip@gmail.com>
2021-04-05 00:40:49 +0300

    Update variable naming, added average filter methods.

--- SpatialFiltering/Filter.cs
+++ SpatialFiltering/Filter.cs
@@ -10,13 +10,13 @@ namespace SpatialFiltering
     {
 
         private readonly Dictionary<string, Action> _filters;
-        public YuvModel _yuv;
-        public readonly Helpers _helper;
+        private readonly YuvModel _yuv;
+        private readonly Helpers _helpers;
 
-        public Filter(YuvModel yuv, Helpers helper)
+        public Filter(YuvModel yuv, Helpers helpers)
         {
             _yuv = yuv;
-            _helper = helper;
+            _helpers = helpers;
             
             _filters = new Dictionary<string, Action>()
             {
@@ -62,13 +62,13 @@ namespace SpatialFiltering
 
             if (_yuv.Dimensions is 1)
             {
-                byte[] YbytesExtended = _helper.Extend1DArray(_yuv.Ybytes, _yuv.YTotalBytes);
+                byte[] YbytesExtended = _helpers.Extend1DArray(_yuv.Ybytes, _yuv.YTotalBytes);
 
 
-                _helper.InformUser();
+                _helpers.InformUser();
 
 
-                Array.Resize(ref _yuv.YMedian, _yuv.YResolution);
+                Array.Resize(ref _yuv.YFiltered, _yuv.YResolution);
 
 
                 Parallel.Invoke(
@@ -117,23 +117,21 @@ namespace SpatialFiltering
                                 }
                 );
 
-
-                //_outputProvider("Task successfully completed.\n");
             }
 
             else if (_yuv.Dimensions is 2)
             {
 
-                _helper.InformUser();
+                _helpers.InformUser();
 
 
                 Parallel.Invoke(
-                    () => _yuv.Yplane = _helper.ConvertTo2DExtendedArray(_yuv.Ybytes, _yuv.YHeight, _yuv.YWidth),
-                    () => _yuv.Uplane = _helper.ConvertTo2DArray(_yuv.Ubytes, _yuv.UHeight, _yuv.UWidth),
-                    () => _yuv.Vplane = _helper.ConvertTo2DArray(_yuv.Vbytes, _yuv.VHeight, _yuv.VWidth));
+                    () => _yuv.Yplane = _helpers.ConvertTo2DExtendedArray(_yuv.Ybytes, _yuv.YHeight, _yuv.YWidth),
+                    () => _yuv.Uplane = _helpers.ConvertTo2DArray(_yuv.Ubytes, _yuv.UHeight, _yuv.UWidth),
+                    () => _yuv.Vplane = _helpers.ConvertTo2DArray(_yuv.Vbytes, _yuv.VHeight, _yuv.VWidth));
 
 
-                _yuv.YMedian2D = new byte[_yuv.YHeight, _yuv.YWidth];
+                _yuv.YFiltered2D = new byte[_yuv.YHeight, _yuv.YWidth];
 
 
                 Parallel.Invoke(
@@ -182,8 +180,6 @@ namespace SpatialFiltering
                                 }
                 );
 
-
-                //_outputProvider("Task successfully completed.\n");
             }
 
 
@@ -195,8 +191,135 @@ namespace SpatialFiltering
 
         public Task ApplyAverageFilter()
         {
+            bool q = false;
+            bool w = false;
 
-            return Task.CompletedTask;
+
+            if (_yuv.Dimensions is 1)
+            {
+                byte[] YbytesExtended = _helpers.Extend1DArray(_yuv.Ybytes, _yuv.YTotalBytes);
+
+
+                _helpers.InformUser();
+
+
+                Array.Resize(ref _yuv.YFiltered, _yuv.YResolution);
+
+
+                Parallel.Invoke(
+                                delegate ()
+                                {
+
+                                    int index = (_yuv.Mask - 1) / 2;
+
+                                    while (index < YbytesExtended.Length - ((_yuv.Mask - 1) / 2))
+                                    {
+                                        q = true;
+
+                                        FindAverage(YbytesExtended, index);
+                                        index++;
+                                    }
+                                },
+
+                                delegate ()
+                                {
+                                    while (true)
+                                    {
+                                        for (int dots = 0; dots < 3; ++dots)
+                                        {
+                                            Console.Write('.');
+                                            Thread.Sleep(500);
+                                            if (dots == 2)
+                                            {
+                                                if (q == true)
+                                                {
+                                                    w = true;
+                                                    break;
+                                                }
+
+                                                // Console.SetCursorPosition(Left, Top);
+                                                Console.Write(new string(' ', 3));
+                                                //Console.SetCursorPosition(Left, Top);
+
+                                                dots = -1;
+                                                Thread.Sleep(500);
+                                            }
+                                        }
+
+                                        if (w == true)
+                                            break;
+                                    }
+                                }
+                );
+
+
+            }
+
+            else if (_yuv.Dimensions is 2)
+            {
+
+                _helpers.InformUser();
+
+
+                Parallel.Invoke(
+                    () => _yuv.Yplane = _helpers.ConvertTo2DExtendedArray(_yuv.Ybytes, _yuv.YHeight, _yuv.YWidth),
+                    () => _yuv.Uplane = _helpers.ConvertTo2DArray(_yuv.Ubytes, _yuv.UHeight, _yuv.UWidth),
+                    () => _yuv.Vplane = _helpers.ConvertTo2DArray(_yuv.Vbytes, _yuv.VHeight, _yuv.VWidth));
+
+
+                _yuv.YFiltered2D = new byte[_yuv.YHeight, _yuv.YWidth];
+
+
+                Parallel.Invoke(
+                                delegate ()
+                                {
+
+                                    for (int i = ((_yuv.Mask - 1) / 2); i < _yuv.Yplane.GetLength(0) - ((_yuv.Mask - 1) / 2); i++)
+                                    {
+                                        for (int j = ((_yuv.Mask - 1) / 2); j < _yuv.Yplane.GetLength(1) - ((_yuv.Mask - 1) / 2); j++)
+                                        {
+                                            FindAverage2D(i, j);
+                                        }
+                                    }
+
+                                    q = true;
+                                },
+
+                                delegate ()
+                                {
+                                    while (true)
+                                    {
+                                        for (int dots = 0; dots < 3; ++dots)
+                                        {
+                                            Console.Write('.');
+                                            Thread.Sleep(500);
+                                            if (dots == 2)
+                                            {
+                                                if (q == true)
+                                                {
+                                                    w = true;
+                                                    break;
+                                                }
+
+                                                //Console.SetCursorPosition(Left, Top);
+                                                Console.Write(new string(' ', 3));
+                                                //Console.SetCursorPosition(Left, Top);
+
+                                                dots = -1;
+                                                Thread.Sleep(500);
+                                            }
+                                        }
+
+                                        if (w == true)
+                                            break;
+                                    }
+                                }
+                );
+
+            }
+
+
+                return Task.CompletedTask;
         }
 
 
@@ -211,8 +334,7 @@ namespace SpatialFiltering
             }
 
             temp.Sort();
-            _yuv.YMedian[index - ((_yuv.Mask - 1) / 2)] = temp.ElementAt((_yuv.Mask - 1) / 2);
-
+            _yuv.YFiltered[index - ((_yuv.Mask - 1) / 2)] = temp.ElementAt((_yuv.Mask - 1) / 2);
         }
 
 
@@ -231,7 +353,42 @@ namespace SpatialFiltering
             }
 
             temp.Sort();
-            _yuv.YMedian2D[i_index - ((_yuv.Mask - 1) / 2), j_index - ((_yuv.Mask - 1) / 2)] = temp.ElementAt((int)((Math.Pow(_yuv.Mask, 2) - 1) / 2));
+            _yuv.YFiltered2D[i_index - ((_yuv.Mask - 1) / 2), j_index - ((_yuv.Mask - 1) / 2)] = temp.ElementAt((int)((Math.Pow(_yuv.Mask, 2) - 1) / 2));
+        }
+
+
+
+        private void FindAverage(byte[] input, int index)
+        {
+            int temp = 0;
+            int i;
+
+            for (i = 0; i < _yuv.Mask; i++)
+                temp += input[index - ((_yuv.Mask - 1) / 2) + i];
+
+            temp /= i;
+
+            _yuv.YFiltered[index - ((_yuv.Mask - 1) / 2)] = (byte)temp;
+        }
+
+
+
+        private void FindAverage2D(int i_index, int j_index)
+        {
+            double temp = 0;
+
+            // Find the correct block on given index and mask
+            for (int i = i_index - ((_yuv.Mask - 1) / 2); i <= i_index + ((_yuv.Mask - 1) / 2); i++)
+            {
+                for (int j = j_index - ((_yuv.Mask - 1) / 2); j <= j_index + ((_yuv.Mask - 1) / 2); j++)
+                {
+                    temp += _yuv.Yplane[i, j];
+                }
+            }
+
+            temp /= Math.Pow(_yuv.Mask, 2);
+
+            _yuv.YFiltered2D[i_index - ((_yuv.Mask - 1) / 2), j_index - ((_yuv.Mask - 1) / 2)] = (byte)temp;
         }
 
 
